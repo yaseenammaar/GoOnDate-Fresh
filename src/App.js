452d4218a749aa3f6054b4f7b675e6cc7dee0a3e
yaseenammaar <yammaar9@gmail.com>
2022-01-06 21:01:31 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
-import { Text } from "react-atomize";
+import {
+  ThemeProvider,
+  Div,
+  Row,
+  Col,
+  DefaultTheme,
+  Image,
+  Button,
+  Icon,
+  Text,
+  Modal,
+  Notification,
+  Dropdown,
+  Anchor
+} from "react-atomize";
+
+const theme = {
+  ...DefaultTheme,
+  grid: {
+      ...DefaultTheme.grid,
+      colCount: 12,
+      gutterWidth: 0
+  }
+};
+
+
 
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <Row
+            m={{t:"1.5rem"}}>
+                <Col size={{xs: 0, lg: 1}}>
+
+                </Col>
+                <Col size={{xs: 1, lg: 1}}>
+                    <Image
+                        src=""
+                        h="2rem"
+                        w="auto"
+                    />
+                    
+                </Col>
+
+                <Col size={{xs: 3, lg: 6}}>
+
+                </Col>
+                <Col size={{xs: 1, lg: 1}}>
+                    
+                             
+                        <Button
+                            h="2.5rem"
+                            p={{x: "1rem"}}
+                            textSize="body"
+                            textColor="gray900"
+                            bg="white"
+                            fontFamily="primary"
+                            m={{r: "0.5rem"}}
+                        >
+                            About
+                        </Button>
+                    
+                </Col>
+
+                <Col size={{xs: 1, lg: 1}}>
+                    <Button
+                        h="2.5rem"
+                        p={{x: "1rem"}}
+                        textSize="body"
+                        textColor="gray900"
+                        bg="white"
+                        fontFamily="primary"
+                        m={{r: "0.5rem"}}
+                    >
+                        Contact
+                    </Button>
+                </Col>
+                <Col size={{xs: 1, lg: 1}}>
+                    
+                        <Button
+                        bg="white"
+                        textColor="gray900"
+                        p={{r: "3rem", l: "3rem"}}
+                        shadow="1"
+                        hoverShadow="2"
+                        fontFamily="primary"
+                    >
+                        Connect Wallet
+                    </Button>
+                </Col>
+                <Col size={{xs: 1, lg: 1}}>
+
+                </Col>
+            </Row>
+    </ThemeProvider>
     
-    <Text>
-      This is h1 of display1 size
-    </Text>
   );
 }
 
